613502c26949235722aee0d4d3fdc009afd8ed22
Tylre-Reamer <tymakesit@gmail.com>
2026-05-28 20:11:27 -0700

    feat: add AppDbContext with EF Core configuration

---
+++ src/FoodDelivery.API/Data/AppDbContext.cs
@@ -0,0 +1,45 @@
+using FoodDelivery.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDelivery.API.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Restaurant> Restaurants => Set<Restaurant>();
+    public DbSet<Meal> Meals => Set<Meal>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+    public DbSet<Coupon> Coupons => Set<Coupon>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        modelBuilder.Entity<Coupon>()
+            .HasIndex(c => c.Code)
+            .IsUnique();
+
+        modelBuilder.Entity<Order>()
+            .Property(o => o.TotalPrice)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Order>()
+            .Property(o => o.Tip)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<OrderItem>()
+            .Property(oi => oi.UnitPrice)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Meal>()
+            .Property(m => m.Price)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Coupon>()
+            .Property(c => c.DiscountValue)
+            .HasPrecision(18, 2);
+    }
+}
