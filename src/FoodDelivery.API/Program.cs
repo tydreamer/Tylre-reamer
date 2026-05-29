78c15467aca3c006bc0afbdc2f086fdd460b490f
Tylre-Reamer <tymakesit@gmail.com>
2026-05-28 20:15:02 -0700

    feat: add EF Core + PostgreSQL packages and wire up DbContext

--- src/FoodDelivery.API/Program.cs
+++ src/FoodDelivery.API/Program.cs
@@ -1,4 +1,10 @@
+using FoodDelivery.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
